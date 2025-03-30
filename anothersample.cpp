6d9d5dd88ec5c40faf3d12679cc4f7b133b33f10
arnav <arnavsinha4334@gmail.com>
2025-03-30 12:59:35 +0530

    added both files

---
+++ anothersample.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello sample2" << std::endl;
+    return 0;
+}
---
+++ sample.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello GitH" << std::endl;
+    return 0;
+}
